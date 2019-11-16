b04445bad929a976eef21ede8ff8bbabaf73f39f
soa-a319-e-008 <soa-a319-e-008@soa-a313-e-008.UMD.LOCAL.EDU>
2019-11-15 21:14:21 -0500

    subir archivos por primera vez a  git hub

---
+++ src/ordenamiento/Ordenamiento.java
@@ -0,0 +1,45 @@
+
+package ordenamiento;
+
+public class Ordenamiento 
+{
+// recibe un arreglo de tipo entero
+    //devuelve vacio
+    
+    //metodo para ordenar
+void sort (int arr[])
+    {
+    //tamaño de arreglo
+ int n = arr.length;
+  //para  recorrerlo
+  for (int i = 0; i< n-1; i++ ){     
+   
+   int min_idx = i;
+     //siempre que voy a hacer arrglos tengo que tener 2 "for" anidados
+    
+    for (int j = i+1; j < n; j ++)
+      //comparar
+      if (arr[j]< arr[min_idx]) 
+         min_idx = j;    
+          
+    //intercambio de menor a mayor
+    
+    int temp = arr [min_idx];
+     arr [min_idx] = arr [i];
+       arr[i] = temp;
+       
+  }
+    }
+    
+public static void main(String[] args) {
+    //crear el arreglo 
+    Ordenamiento Ob = new Ordenamiento();
+    Ob.sort(arr);
+    
+    
+    int arr[] = {64,25,12,22,11};
+    
+
+}
+    
+}
